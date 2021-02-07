726a766d8542f365e3a3501fbcf803ece780c7c1
Jordi Casanella <jordikiter@hotmail.es>
2021-02-07 21:44:29 +0000

    Build first version of the docker image

---
+++ src/main/java/com/learn/jmx/agent/Agent.java
@@ -0,0 +1,30 @@
+package com.learn.jmx.agent;
+
+import com.learn.jmx.metrics.SystemConfig;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
+public class Agent {
+
+    private static final int DEFAULT_NO_THREADS=10;
+    private static final String DEFAULT_SCHEMA="default";
+
+    public static void main(String[] args) {
+        try {
+            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+
+            SystemConfig mBean = new SystemConfig(DEFAULT_NO_THREADS, DEFAULT_SCHEMA);
+            ObjectName name = new ObjectName("com.learn.jmx.metrics:type=SystemConfig");
+            mbs.registerMBean(mBean, name);
+
+            do{
+                Thread.sleep(3000);
+                System.out.println("Thread Count="+mBean.getThreadCount()+":::Schema Name="+mBean.getSchemaName());
+            }while(mBean.getThreadCount() !=0);
+
+        } catch (MalformedObjectNameException | InstanceAlreadyExistsException | MBeanRegistrationException | NotCompliantMBeanException | InterruptedException ex) {
+            System.out.println("Error!!!");
+        }
+    }
+}
